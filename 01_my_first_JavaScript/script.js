83633249d15127d3021e91d0e8f0b5388e7cc3f1
Jonnie <jonnie.muis@gmail.com>
2020-10-30 16:39:13 +0100

    Add files via upload

---
+++ 01_my_first_JavaScript/script.js
@@ -0,0 +1,22 @@
+//Opdracht 1
+console.log('Hello Winc Academy');
+//Opdracht 2
+let name = 'Jonnie';
+console.log(name);
+//Opdracht 3
+// let if werkt niet omdat 'if' een reserved keyword is
+//Opdracht 4
+console.log(4 + 4);
+console.log('4' + '4');
+//Opdracht 5
+console.log(2 + 4);
+console.log(4 - 2);
+console.log(2 * 4);
+console.log(4 / 2);
+console.log(4 % 2);
+//Opdracht 6
+let age = 37;
+console.log(typeof age);
+//Opdracht 7
+age = '37';
+console.log(typeof age);
